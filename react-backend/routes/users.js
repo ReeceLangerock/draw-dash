3cf3b09075088eaa7bc53bfea824ffb50012d3bf
ReeceLangerock <ReeceLangerock@gmail.com>
2017-06-21 23:04:41 -0500

    adding minimally working backend branch

---
+++ react-backend/routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	// Comment out this line:
+  //res.send('respond with a resource');
+  console.log('test')
+  // And insert something like this instead:
+  res.json([{
+  	id: 1,
+  	username: "samsepi0l"
+  }, {
+  	id: 2,
+  	username: "D0loresH4ze"
+  },
+  {
+    id: 3,
+    username: "reece"
+  }]);
+});
+
+module.exports = router;
