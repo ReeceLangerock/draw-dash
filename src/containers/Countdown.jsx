b406257bf5dc9dfd96cebd43864cf32f6bfe375c
wildlifehexagon <crossthebreeze@gmail.com>
2017-07-20 17:14:53 -0500

    revert back to working timer to prep for merge

--- src/containers/Countdown.jsx
+++ src/containers/Countdown.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom"
 import Navigation from './Navigation';
 
 class Countdown extends React.Component {
   constructor(props) {
     super(props);
 
-    // still need to hide start button
+    // still need to add 3-5 second timer before the real countdown
+    // and hide start button
 
-    this.state = { time: {}, seconds: 3 };
+    this.state = { time: {}, seconds: 60 };
     this.timer = 0;
     this.startTimer = this.startTimer.bind(this);
     this.countdown = this.countdown.bind(this);
@@ -54,26 +54,7 @@ class Countdown extends React.Component {
 
     if (seconds === 0) {
       clearInterval(this.timer);
-      setTimeout(() => {
-        let element = (
-          <h2>GO!</h2>
-        );
-        ReactDOM.render(
-          element,
-          document.getElementByClassName('title')
-        );
-      }, 1000);
     }
-  }
-
-  longCountdown() {
-    this.state.seconds = 61;
-    let seconds = this.state.seconds - 1;
-    this.timer = setInterval(this.countdown, 1000);
-    this.setState({
-      time: this.formatSeconds(seconds),
-      seconds: seconds
-    });
   }
 
   render() {
@@ -82,7 +63,7 @@ class Countdown extends React.Component {
         <Navigation />
         <div className="row">
           <div className="column small-centered medium-6 large-4">
-            <h2 className="title">Countdown Component</h2>
+            <h2>Countdown Component</h2>
             {this.state.time.seconds}<br />
             <button className="button" onClick={this.startTimer}>Start</button>
           </div>
