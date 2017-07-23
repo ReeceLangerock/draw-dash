63e16aad67fdc37c9cdf9a674192ff2afc9f3dbf
wildlifehexagon <crossthebreeze@gmail.com>
2017-07-22 22:16:52 -0500

    got countdown/timer properly working

--- src/containers/Countdown.jsx
+++ src/containers/Countdown.jsx
@@ -1,82 +1,80 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom"
 import Navigation from './Navigation';
-import LongCountdown from './LongCountdown';
 
 class Countdown extends React.Component {
   constructor(props) {
     super(props);
 
-    // still need to hide start button
-
-    this.state = { time: {}, seconds: 3 };
-    this.timer = 0;
-    this.startTimer = this.startTimer.bind(this);
-    this.countdown = this.countdown.bind(this);
-  }
-
-  formatSeconds (totalSeconds) {
-    let seconds = totalSeconds;
-    let minutes = Math.floor(totalSeconds / 60);
-
-    if (seconds < 10) {
-      seconds = '0' + seconds;
-    }
-
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-
-    let obj = {
-      "minutes": minutes,
-      "seconds": seconds
+    this.state = {
+      countdownSeconds: 3,
+      roundSeconds: 60
     };
 
-    return obj;
-  }
-
-  componentDidMount() {
-    let timeLeft = this.formatSeconds(this.state.seconds);
-    this.setState({ time: timeLeft });
+    this.roundCountdown = this.roundCountdown.bind(this);
+    this.countdown = this.countdown.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.countdownTimer = null;
+    this.roundTimer = null;
   }
 
   startTimer() {
-    if (this.timer === 0) {
-      this.timer = setInterval(this.countdown, 1000);
+    if (!this.countdownTimer) {
+      this.countdownTimer = setInterval(this.countdown, 1000);
     }
   }
 
   countdown() {
-    let seconds = this.state.seconds - 1;
+    const currentSeconds = this.state.countdownSeconds - 1;
+
     this.setState({
-      time: this.formatSeconds(seconds),
-      seconds: seconds
+      countdownSeconds: currentSeconds
     });
 
-    if (seconds === 0) {
-      clearInterval(this.timer);
-      setTimeout(() => {
-        let element = (
+    if (currentSeconds === 0) {
+      this.roundTimer = setInterval(this.roundCountdown, 1000);
+      clearInterval(this.countdownTimer);
+      let element = (
           <h2>GO!</h2>
         );
         ReactDOM.render(
           element,
           document.getElementById('go')
         );
-      }, 1000);
-
     }
   }
 
+  roundCountdown() {
+    const currentSeconds = this.state.roundSeconds - 1;
+
+    this.setState({
+      roundSeconds: currentSeconds
+    });
+
+    if (currentSeconds === 0) {
+      clearInterval(this.roundTimer);
+    }
+  }
+
+  renderTimer() {
+    const { countdownSeconds, roundSeconds } = this.state;
+
+    if (countdownSeconds > 0) {
+      return <div>Counting down...<br/>{countdownSeconds}</div>;
+    }
+
+    return <div>{roundSeconds}</div>
+  }
+
   render() {
+    const { countdownSeconds, roundSeconds } = this.state;
     return(
       <div className="container">
         <Navigation />
         <div className="row">
           <div className="column small-centered medium-6 large-4">
             <h2 id="go"></h2>
-            {this.state.time.seconds}<br />
-            <LongCountdown />
+            {this.renderTimer()}
             <button className="button" onClick={this.startTimer}>Start</button>
           </div>
         </div>
