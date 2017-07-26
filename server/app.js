e71a99d11558128e643c6a5de1fbf17f06504c83
ReeceLangerock <ReeceLangerock@gmail.com>
2017-07-26 17:24:19 -0500

    heroku test

--- server/app.js
+++ server/app.js
@@ -69,6 +69,12 @@ app.use("/api/room", require("./routes/room")(io, socketRooms, imagePrompts));
 app.use("/api/gallery", require("./routes/gallery"));
 app.use("/api/leaderboard", require("./routes/leaderboard"));
 
+app.get('*', (req, res)=> {
+  console.log('get');
+  console.log(path.join(__dirname+'/build/index.html'));
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+})
+
 
 //launch
 server.listen(port, function() {
