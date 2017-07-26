3e424eebedec485a099c7a3c123031e4b894963b
wildlifehexagon <crossthebreeze@gmail.com>
2017-07-25 22:18:57 -0500

    add react-color module + clean up code per eslinter

--- src/containers/Canvas.jsx
+++ src/containers/Canvas.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-import Navigation from "./Navigation";
-import { Layer, Stage, Image } from "react-konva";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import React from 'react';
+import Navigation from './Navigation';
+import { Layer, Stage, Image } from 'react-konva';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { GithubPicker } from 'react-color';
 
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
     this.canvasEvent = this.canvasEvent.bind(this);
-    this.props.socket.on("canvas_update", data => {
-      var idToUpdate = data.canvasId == 1 ? "drawing2" : "drawing1";
-      var node = document.createElement("p");
-      var textNode = document.createTextNode("test");
+    this.props.socket.on('canvas_update', data => {
+      const idToUpdate = data.canvasId == 1 ? 'drawing2' : 'drawing1';
+      const node = document.createElement('p');
+      const textNode = document.createTextNode('test');
       node.appendChild(textNode);
       document.getElementById(idToUpdate).appendChild(node);
       console.log(data.canvasJSON);
@@ -19,90 +20,98 @@ class Canvas extends React.Component {
 
       //this.renderDisabledKonva(undefined, data.canvasJSON, data.canvasId);
     });
+    this.state = {
+      background: '#fff',
+    };
   }
   canvasEvent(canvasJSON) {
-    this.props.socket.emit("canvas_event", { roomId: this.props.roomId, canvasJSON: canvasJSON, canvasId: this.props.canvasId }, function() {});
+    this.props.socket.emit('canvas_event', { roomId: this.props.roomId, canvasJSON: canvasJSON, canvasId: this.props.canvasId }, () => {});
   }
 
+  handleChangeComplete = (color) => {
+    this.setState({ background: color.hex });
+  };
+
   renderTools() {
-    if (true) {
-      return (
-        <div className="tool">
-          Tool:
-          <select id="tool">
-            <option value="brush">Brush</option>
-            <option value="eraser">Eraser</option>
-          </select>
-        </div>
-      );
-    }
+    return (
+      <div className="tool">
+        Tool:
+        <select id="tool">
+          <option value="brush">Brush</option>
+          <option value="eraser">Eraser</option>
+        </select>
+      </div>
+    );
   }
 
   renderKonva(container) {
-    var that = this;
-    var width = 400; //window.innerWidth;
-    var height = 400; //window.innerHeight - 25;
+    const that = this;
+    const width = 400; // window.innerWidth;
+    const height = 400; // window.innerHeight - 25;
     // first we need Konva core things: stage and layer
-    var stage = new Konva.Stage({
+    const stage = new Konva.Stage({
       container: `drawing${this.props.canvasId}`,
       width: width,
       height: height
     });
-    var layer = new Konva.Layer({});
+    const layer = new Konva.Layer({});
     stage.add(layer);
     // then we are going to draw into special canvas element
-    var canvas = document.createElement("canvas");
+    const canvas = document.createElement('canvas');
     canvas.width = stage.width() / 1.5;
     canvas.height = stage.height() / 1.5;
-    // created canvas we can add to layer as "Konva.Image" element
-    var image = new Konva.Image({
+    // created canvas we can add to layer as 'Konva.Image' element
+    let image = new Konva.Image({
       image: canvas,
       x: stage.width() / 6,
       y: stage.height() / 6,
-      stroke: "#05AFF2",
-      shadowBlur: 5
+      stroke: '#05AFF2',
+      shadowBlur: 5,
     });
     layer.add(image);
     stage.draw();
     // Good. Now we need to get access to context element
-    var context = canvas.getContext("2d");
-    context.strokeStyle = "#000000";
-    context.lineJoin = "round";
+    const context = canvas.getContext('2d');
+    context.strokeStyle = this.props.background;
+    console.log(context.strokeStyle);
+    context.lineJoin = 'round';
     context.lineWidth = 5;
-    var isPaint = false;
-    var lastPointerPosition;
-    var mode = "brush";
+    let isPaint = false;
+    let lastPointerPosition;
+    let mode = 'brush';
     // now we need to bind some events
     // we need to start drawing on mousedown
     // and stop drawing on mouseup
-    stage.on("contentMousedown.proto", function() {
+    stage.on('contentMousedown.proto', () => {
       isPaint = true;
       lastPointerPosition = stage.getPointerPosition();
     });
-    stage.on("contentMouseup.proto", function() {
+    stage.on('contentMouseup.proto', () => {
       isPaint = false;
     });
     // and core function - drawing
-    stage.on("contentMousemove.proto", function() {
+    stage.on('contentMousemove.proto', () => {
       if (!isPaint) {
         return;
       }
-      if (mode === "brush") {
-        context.globalCompositeOperation = "source-over";
+      if (mode === 'brush') {
+        context.lineWidth = 5;
+        context.globalCompositeOperation = 'source-over';
       }
-      if (mode === "eraser") {
-        context.globalCompositeOperation = "destination-out";
+      if (mode === 'eraser') {
+        context.lineWidth = 15;
+        context.globalCompositeOperation = 'destination-out';
       }
       context.beginPath();
-      var localPos = {
+      let localPos = {
         x: lastPointerPosition.x - image.x(),
-        y: lastPointerPosition.y - image.y()
+        y: lastPointerPosition.y - image.y(),
       };
       context.moveTo(localPos.x, localPos.y);
-      var pos = stage.getPointerPosition();
+      const pos = stage.getPointerPosition();
       localPos = {
         x: pos.x - image.x(),
-        y: pos.y - image.y()
+        y: pos.y - image.y(),
       };
       context.lineTo(localPos.x, localPos.y);
       context.closePath();
@@ -110,15 +119,14 @@ class Canvas extends React.Component {
       lastPointerPosition = pos;
       layer.draw();
 
-        var dataURL = stage.toDataURL();
-        // window.open(dataURL);
-        that.canvasEvent(dataURL);
-        // that.canvasEvent(stage.toJSON());
-        // console.log(layer);
-
+      const dataURL = stage.toDataURL();
+      // window.open(dataURL);
+      that.canvasEvent(dataURL);
+      // that.canvasEvent(stage.toJSON());
+      // console.log(layer);
     });
-    var select = document.getElementById("tool");
-    select.addEventListener("change", function() {
+    const select = document.getElementById('tool');
+    select.addEventListener('change', () => {
       mode = select.value;
     });
   }
@@ -127,7 +135,8 @@ class Canvas extends React.Component {
     return (
       <div className="container">
         {this.renderTools()}
-        <div id={"drawing" + this.props.canvasId} ref={ref => this.renderKonva(ref)} />
+        <GithubPicker onChangeComplete={this.handleChangeComplete} color={this.state.background} />
+        <div id={'drawing' + this.props.canvasId} ref={ref => this.renderKonva(ref)} />
       </div>
     );
   }
@@ -136,7 +145,7 @@ class Canvas extends React.Component {
 const mapStateToProps = state => ({
   isAuthenticated: state.authReducer.isAuthenticated,
   user: state.authReducer,
-  roomId: state.roomReducer.currentUserRoom
+  roomId: state.roomReducer.currentUserRoom,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
