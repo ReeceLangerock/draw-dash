be9287a249b810a7e731aec9700e29e7b7ac55c4
ReeceLangerock <ReeceLangerock@gmail.com>
2017-07-26 00:19:56 -0500

    last routing fix try before bed

--- server/app.js
+++ server/app.js
@@ -51,8 +51,10 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-//app.use(express.static( __dirname + '/../build'));
-app.use(express.static(path.resolve(`${__dirname}./../build`)));
+app.use(express.static( __dirname + '/../build'));
+app.use(express.static( __dirname + 'build'));
+
+//app.use(express.static(path.resolve(`${__dirname}./../build`)));
 //ROUTES
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
